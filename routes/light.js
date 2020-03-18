7addb4a1de12621aa067011c2b939dd844461a0d
Gustavo Ventura <gustavo.ventura100@gmail.com>
2020-03-18 17:36:07 -0300

    Ajuste na rota light e no view engine setup

--- routes/light.js
+++ routes/light.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var router = express.Router();
-//const Gpio = require('onoff').Gpio;
-//const LED = new Gpio(4,'out');
+const Gpio = require('onoff').Gpio;
+const LED = new Gpio(4, 'out');
 
 //acende o led conectado no pino 4 da raspberry
 router.post('/on', function (req, res, next) {
-    //  LED.writeSync(1);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    LED.writeSync(1);
+    console.log("Led Aceso");
     res.json({ 'status': 'OK', 'data': 'Led Aceso' });
 });
 
 //apaga o led conectado no pino da raspberry
 router.post('/off', function (req, res, next) {
-    //LED.writeSync(0);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    LED.writeSync(0);
+    console.log("Led Apagado");
     res.json({ 'status': 'OK', 'data': 'Led Apagado' });
 });
 
